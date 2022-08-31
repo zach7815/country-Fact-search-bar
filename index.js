5f0089f53ee8ed1abe4114b8bcb5253f0abfe6e5
zach7815 <mcfeegles@gmail.com>
2022-08-31 16:18:58 +0100

    added fetch function, and worked on grabbing target data for cards, changed styling for better readability

---
+++ index.js
@@ -0,0 +1,59 @@
+
+const apiEndpoint = `https://restcountries.com/v3.1/all`;
+const countryTemplate= document.querySelector("[data-country-template]")
+const randomInt = (max)=>{ return Math.floor(Math.random()*max)}
+const countryCardsContainer= document.querySelector("[data-country-cards-container]")
+
+
+
+
+const loadCountries= async()=>{ await fetch(apiEndpoint, 
+    {
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin':'*'
+        }
+    })
+.then(response=>{
+    if(!response.ok){
+        throw response.status
+    }
+
+   return response.json()
+})
+.then(data=>{
+    data.map(country => {
+      
+      
+      
+      
+        const card= countryTemplate.content.cloneNode(true).children[0]
+        const countryDetails ={
+            countryName: card.querySelector("[data-country-header]"),
+            countryFlag:card.querySelector("[data-country-flag]"),
+            officialName:card.querySelector("[data-country-official-name]"),
+            population:card.querySelector("[data-population]"),
+            capitalCity:card.querySelector("[data-capital-city]"),
+            currencyName:card.querySelector("[data-currency-name]"),
+            currencySymbol:card.querySelector("[data-curreny-symbol]"),
+            continent: card.querySelector("[data-continent]")
+        }
+        
+    
+
+        countryDetails.countryName.textContent=country.name.common;
+        countryDetails.countryFlag.src=country.flags.png;
+        countryDetails.officialName.textContent=`Official Name: ${country.name.official}`;
+        countryDetails.population.textContent=`population: ${country.population}`;
+        countryDetails.capitalCity.textContent= `capital: ${country.capital[0]}`
+        
+        
+
+        countryCardsContainer.append(card)
+
+    });
+})
+.catch(err=>console.log(err));
+};
+
+loadCountries();
