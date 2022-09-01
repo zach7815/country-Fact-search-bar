fad8e6575db680167d41e370784c24228093dde5
zach7815 <53745970+zach7815@users.noreply.github.com>
2022-09-01 22:01:28 +0100

    Added search capability, next steps - refactoring and refining

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const apiEndpoint = `https://restcountries.com/v3.1/all`;
 const countryTemplate= document.querySelector("[data-country-template]")
 const randomInt = (max)=>{ return Math.floor(Math.random()*max)}
 const countryCardsContainer= document.querySelector("[data-country-cards-container]")
+const searchInput = document.querySelector('[data-search]')
+
+
+let places=[];
+searchInput.addEventListener("input", (e)=>{
+    const value=e.target.value
+    console.log(value)
+places.forEach( country=>{
+
+   let isVisible=country.name.includes(value)||country.officialName.includes(value)
+   country.card.classList.toggle("hidden", isVisible);
+}
+)
+ 
+   
+    
+})
+
 
 
 
@@ -22,7 +40,8 @@ const loadCountries= async()=>{ await fetch(apiEndpoint,
    return response.json()
 })
 .then(data=>{
-    data.map(country => {
+  
+     places= data.map(country => {
       
       
       
@@ -35,22 +54,32 @@ const loadCountries= async()=>{ await fetch(apiEndpoint,
             population:card.querySelector("[data-population]"),
             capitalCity:card.querySelector("[data-capital-city]"),
             currencyName:card.querySelector("[data-currency-name]"),
-            currencySymbol:card.querySelector("[data-curreny-symbol]"),
+            currencySymbol:card.querySelector("[data-currency-symbol]"),
             continent: card.querySelector("[data-continent]")
         }
         
     
-
         countryDetails.countryName.textContent=country.name.common;
         countryDetails.countryFlag.src=country.flags.png;
         countryDetails.officialName.textContent=`Official Name: ${country.name.official}`;
         countryDetails.population.textContent=`population: ${country.population}`;
-        countryDetails.capitalCity.textContent= `capital: ${country.capital}`
-        countryDetails.currencyName.textContent=`currency: ${country.currencies.EUR||{}.name}`
-        countryDetails.continent.textContent=`continent: ${country.continents}`
-        
+        countryDetails.capitalCity.textContent= `capital: ${country.capital}`;
+        countryDetails.continent.textContent=`continent: ${country.continents}`;
 
+      const currencyKey=Object.keys(null||country.currencies);
+      currencyKey.map(key=>{
+       countryDetails.currencyName.textContent=  `Currency Name: ${country.currencies[key].name}`
+       countryDetails.currencySymbol.textContent=   `Currency Symbol: ${country.currencies[key].symbol}`
+   
+      })
         countryCardsContainer.append(card)
+        const countryFilters={
+            name:countryDetails.countryName.textContent,
+            officialName:countryDetails.officialName.textContent,
+            capital:countryDetails.capitalCity.textContent,
+            continent:countryDetails.continent.textContent
+        }
+        places.push(countryFilters);
 
     });
 })
